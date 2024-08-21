b7aab6d4fa70c72755ea3db680899039845f6223
Patricio Lopez <lopzpdav@gmail.com>
2024-08-21 13:46:01 -0500

    PLOPEZ - RETO TECNICO SCOTIABANK

--- src/main/java/com/springboot/retotecnico/RetoTecnicoApplication.java
+++ src/main/java/com/springboot/retotecnico/RetoTecnicoApplication.java
@@ -1,12 +1,7 @@
 package com.springboot.retotecnico;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
